449d3a35fb25876085dc43ccf3157536ee4257ed
Eetu Eskelinen <eetuesk@gmail.com>
2021-06-22 13:53:55 +0300

    comment out portfolio card temporarily. need to move components to another directory

--- pages/components/PortfolioCard.js
+++ pages/components/PortfolioCard.js
@@ -1,14 +1,14 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 
-export default function PortfolioCard(projectName, projectId) {
-    return(
-        <div className="project_item_container wow animate__animated fadeInUpSmall">
-            <Link href={`/projects/${projectName}`}>
-                <a>
-                    <div className="label">{projectName}</div>
-                    <div className="old_project_item" id={ projectId }></div>
-            </a>
-            </Link>
-          </div>
-    )
-}
+// export default function PortfolioCard(projectName, projectId) {
+//     return (
+//         <div className="project_item_container wow animate__animated fadeInUpSmall">
+//             <Link href={`/projects/${projectName}`}>
+//                 <a>
+//                     <div className="label">{projectName}</div>
+//                     <div className="old_project_item" id={ projectId }></div>
+//             </a>
+//             </Link>
+//           </div>
+//     )
+// }
