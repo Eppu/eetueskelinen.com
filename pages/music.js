0ee21172c1d37c2ebec5e76f07ebab51822c45b0
Eetu Eskelinen <edwardtehgreat@gmail.com>
2021-06-23 20:23:56 +0300

    Add currently playing and improve styles

--- pages/music.js
+++ pages/music.js
@@ -10,32 +10,37 @@ function Music({ data, error }) {
     { refetchOnMount: true }
   );
 
-  let artists = data.artists.items;
-  let recentlyPlayed = data.recentlyPlayed.items;
-  let topSongs = data.songs.items;
-
-  console.log(data);
+  console.log(currentlyPlaying);
 
   if (error || currentError) {
     return <div>There was an error fetching data from spotify</div>;
   }
 
+  let artists = data.artists.items;
+  let recentlyPlayed = data.recentlyPlayed.items;
+  let topSongs = data.songs.items;
+
   return (
     <Layout>
       <>
         <div id="sandbox_header">
-          <h3>Music</h3>
-          <p>Being a big music nerd equates to listening to a lot of different stuff.</p>
-          <b>Here are some of my recent favorites.</b>
+          <h3 className="animate__animated fadeInUpSmall">Music</h3>
+          <p className="animate__animated fadeInUpSmall delay-100ms">
+            Being a big music nerd equates to listening to a lot of different stuff.
+          </p>
+          <p className="animate__animated fadeInUpSmall delay-100ms">
+            <strong>Here are some of my recent favorites.</strong>
+          </p>
         </div>
 
         <div id="music_container">
           {/* Display artists */}
           <div className="project_item_container">
-            <div className="artists_container">
+            <h4 className="animate__animated animate__fadeInUp delay-100ms">Top artists</h4>
+            <div className="artists_container delay-250ms animate__animated fadeInUpSmall">
               {artists.map((artist) => (
                 <div className="music_item artist">
-                  <a href={`https://open.spotify.com/artist/${artist.id}`} target="_blank">
+                  <a href={`https://open.spotify.com/artist/${artist.id}`} target="_blank" rel="noreferrer">
                     <Image
                       alt={"A promo picture of " + artist.name}
                       src={artist.images.filter((image) => image.height >= 150).slice(-1)[0].url}
@@ -49,9 +54,31 @@ function Music({ data, error }) {
             </div>
           </div>
           <div className="project_item_container">
-            <a href="https://github.com/eppu/shrtnr" target="_blank">
-              <div className="music_item song"></div>
-            </a>
+            <h4 className="animate__animated animate__fadeInUp delay-100ms">Currently playing</h4>
+            <div className="currently_playing_container">
+              {/* This can also be a playing_item */}
+              <div className="music_item currently_playing delay-250ms animate__animated fadeInUpSmall">
+                {/* If a song is currently playing... */}
+                {currentlyPlaying && currentlyPlaying.isPlaying ? (
+                  <>
+                    <a href={currentlyPlaying.songUrl} target="_blank" rel="noreferrer">
+                      <Image src={currentlyPlaying.albumImageUrl} height="150px" width="150px" fixed></Image>
+                      <div className="song_info">
+                        <strong>{currentlyPlaying.name}</strong>
+                        <div className="song_details">
+                          <p className="artist_name">{currentlyPlaying.artist}</p>
+                          <p className="album_name">{currentlyPlaying.album}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </>
+                ) : (
+                  <div className="not_playing">
+                    <p>Nothing playing 😴</p>
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </>
@@ -66,7 +93,6 @@ export async function getStaticProps() {
     error = `There was an error: ${response.status}`;
   }
   const data = await response.json();
-  console.log(data);
   return { props: { data, error, revalidate: 60 } };
 }
 
