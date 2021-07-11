5705326901c331acaa2deb82a6fa8ecc249ca70c
Eetu Eskelinen <edwardtehgreat@gmail.com>
2021-07-12 02:38:02 +0300

    Rename experiments to etc

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,8 +54,8 @@ export default function Header() {
           {/* <div class="col-xs-1"></div>  */}
           <div id="nav_links" className="col-xs-7" style={{ zIndex: 9999 }}>
             <Link href="/about">About</Link>
-            <Link href="/experiments">Experiments</Link>
             <Link href="/music">Music</Link>
+            <Link href="/etc">Etc</Link>
           </div>
           {/* <div class="col-xs-1"></div>  */}
         </div>
