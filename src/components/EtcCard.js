338c1b407a0a8235226941eada295f165fd1727c
Eetu Eskelinen <edwardtehgreat@gmail.com>
2021-10-03 02:26:47 +0300

    Remove console log

--- src/components/EtcCard.js
+++ src/components/EtcCard.js
@@ -1,14 +1,13 @@
 function EtcCard(props) {
-    const imgPath = `url("${props.image}")`;
-    console.log(imgPath);
-    return(
+  const imgPath = `url("${props.image}")`;
+  return (
     <div className="wow project_item_container animate__animated fadeInUpSmall delay-250ms">
-        <a href={props.url} target="_blank">
-            <div className="label">{props.label}</div>
-            <div className="old_project_item" id={props.id} style={{backgroundImage: imgPath}}></div>
-        </a>
-     </div>
-    )
+      <a href={props.url} target="_blank">
+        <div className="label">{props.label}</div>
+        <div className="old_project_item" id={props.id} style={{ backgroundImage: imgPath }}></div>
+      </a>
+    </div>
+  );
 }
 
 export default EtcCard;
