1be54e8188bbebf68ee735e82d878eb9bd335c57
Eetu Eskelinen <eetuesk@gmail.com>
2021-10-21 16:10:44 +0300

    Minor CSS tweaks

--- pages/etc.js
+++ pages/etc.js
@@ -19,7 +19,11 @@ export default function Etc() {
     <Layout title="Etc">
       <div id="sandbox_header">
         <h1 className="about_title animate__animated fadeInUpSmall">Etc</h1>
-        <h4 className="animate__animated fadeInUpSmall delay-100ms">
+        <h4 
+          className="animate__animated fadeInUpSmall delay-100ms"
+          // Override regular styles 
+          style={{fontWeight: "400", fontSize: "1em", fontStyle: "normal", color: "#414141"}}
+        >
           I love learning new things in and around the tech industry, and tend to do that by working on experimental
           projects. <br />
           <b>Here are a few of the latest ones.</b>
