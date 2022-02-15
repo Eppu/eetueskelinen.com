f539061a3c06d2478de675e964e59e42c2275b0c
Eetu Eskelinen <eetuesk@gmail.com>
2022-02-15 17:37:53 +0200

    Add custom transformation functions
    Update transform
    
    Transform mods
    
    Fix sitemap dynamic transform
    
    Ok, fix dynamic transforms for real this time.

--- next-sitemap.js
+++ next-sitemap.js
@@ -4,4 +4,24 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://eetueskelinen.com",
   generateRobotsTxt: true, // (optional)
   // ...other options
+
+  transform: async (config, path) => {
+    // If the current page is `pages/index.js`, set priority to 1
+    if (path === "/") {
+      return {
+        loc: path,
+        changefreq: config.changefreq,
+        priority: 1.0,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      };
+    }
+
+    // Use defaults for all other cases
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+  },
 };
