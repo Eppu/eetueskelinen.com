c2635e2aed1c4b38dbb3962488b835547ce3ce02
Eetu Eskelinen <eetuesk@gmail.com>
2022-09-08 21:49:01 +0300

    Remove comment

--- src/components/EtcCard.js
+++ src/components/EtcCard.js
@@ -6,11 +6,7 @@ function EtcCard(props) {
     <div className="project_item_container animate__animated fadeInUpSmall delay-250ms">
       <a href={props.url} target="_blank">
         <div className="label">{props.label}</div>
-        <div
-          className="old_project_item"
-          id={props.id}
-          // style={{ backgroundImage: imgPath }}
-        >
+        <div className="old_project_item" id={props.id}>
           <Image
             src={props.image}
             alt={props.label}
