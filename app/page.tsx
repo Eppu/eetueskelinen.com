4536980979fa5ed5875b45734ed7048d1cfae677
Eetu Eskelinen <eetuesk@gmail.com>
2024-04-19 16:18:12 +0300

    Hero text mobile improvements

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import { playfairDisplay } from "@/app/utils/fonts";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-16 max-w-7xl">
+    <section className="flex min-h-screen flex-col justify-between md:p-16 max-w-7xl">
       <div className="max-w-5xl">
-        <h1 className={`${playfairDisplay.className} text-7xl leading-semi-tight`}>
+        <h1 className={`${playfairDisplay.className} md:text-7xl text-5xl leading-semi-tight`}>
           <b className="italic">Eetu</b> is a software developer excited about solving human problems using technology.
         </h1>
-        <p className="text-2xl mt-8">
+        <p className="md:text-2xl text-xl mt-8">
           He is passionate about building products that make a difference and is always looking for new ways to learn
           and grow.
         </p>
       </div>
-    </main>
+    </section>
   );
 }
