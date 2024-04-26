9574427c515d0c390d4a6eec97e9fda879006f55
Eetu Eskelinen <eetuesk@gmail.com>
2024-04-26 12:35:33 +0300

    Work

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,25 @@
+import { playfairDisplay } from "../utils/fonts";
+import Image from "next/image";
+
 export default function About() {
   return (
     <section className="flex min-h-screen flex-col md:py-16 justify-between max-w-7xl">
-      <div className="max-w-5xl ">This is my about page. Todo!</div>
+      <div className="max-w-5xl">
+        <h1 className={`${playfairDisplay.className} md:text-5xl md:leading-tight text-5xl leading-tight`}>
+          Hello, I'm Eetu Eskelinen 👋🏻
+        </h1>
+      </div>
+      <div className="flex flex-row">
+        <div>
+          <p className="md:text-2xl text-xl mt-16 ">
+            I'm a software developer passionate about building products that make a difference. I'm always looking for
+            new ways to learn and grow.
+          </p>
+        </div>
+        <div className="md:ml-8">
+          <img src="/images/eetu1.jpg" alt="Eetu Eskelinen" className="rounded-lg" />
+        </div>
+      </div>
     </section>
   );
 }
