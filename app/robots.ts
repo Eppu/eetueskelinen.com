1a952cd590d000d20c74233083e452702c3ef098
Eetu Eskelinen <eetuesk@gmail.com>
2024-06-27 18:46:16 +0300

    Add robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/files/",
+      },
+    ],
+    sitemap: "https://eetueskelinen.com/sitemap.xml",
+  };
+}
